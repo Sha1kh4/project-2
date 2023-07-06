bbf75909f5d7f213b59ea0931324f4fc2067eadc
Sha1kh4 <adnan.shaikh66@mit.asia>
2023-07-06 23:05:33 +0530

    updated app.py to access dash and admin

--- app.py
+++ app.py
@@ -1,34 +1,137 @@
-from flask import Flask, render_template
+from flask import Flask, session, request, g, render_template, redirect, url_for,flash
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
-
+from flask_mysqldb import MySQL
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-app.config['SECRET_KEY'] = '123'
+# import sqlite3  
+# con = sqlite3.connect("employee.db")  
 
+app.secret_key = "1234"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/flask' 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'flask'
+mysql = MySQL(app)
+ 
+class contents(db.Model):
+    no = db.Column(db.Integer, primary_key=True)
+    bgimg = db.Column(db.String(255), nullable=False)
+    href1 = db.Column(db.String(255), nullable=False)
+    title = db.Column(db.String(20), nullable=False)
+    href2 = db.Column(db.String(255), nullable=False)
+    info = db.Column(db.String(1024), nullable=False)
 
-class User(db.Model, UserMixin):
+
+class user(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False,unique=True)
-    password = db.Column(db.String(20), nullable=False)
-
-
+    email = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(50), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    lavel = db.Column(db.Integer,  nullable=False)
+    
 @app.route("/")
-def hello_world():
-    return render_template("index.html", title="Index")
+def index():
+    content = contents.query.all()
 
-@app.route("/login")
+    return render_template("index.html", title="Index", content=content)
+
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html", title="Login")
+    if 'user' not in session:
+        cur = mysql.connection.cursor()
+        error = None
 
-@app.route("/signup")
+        if request.method == 'POST':
+            session.pop('user',None)
+            username = request.form.get("username")
+            password = request.form.get("password")
+            query= "SELECT * FROM user WHERE username ='{}'AND password ='{}'".format(username,password)
+            cur.execute(query) 
+            result=cur.fetchall()
+            current = result[-1]
+            lavel = current[-1]
+            session['lavel']=lavel 
+            if len(result)==0:
+                flash('Invalid Credentials. Please try again.')
+            else:
+                session['user']=username
+                return redirect(url_for("index"))
+        return render_template("login.html", title="Login",error=error)
+    return redirect(url_for("index"))
+   
+
+@app.route("/signup", methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        email = request.form.get('Email')
+        username = request.form.get('username')
+        password = request.form.get('password')
+        entry = user(email=email, username=username, password=password)
+        db.session.add(entry)
+        db.session.commit()
     return render_template("signup.html", title="Sign up")
 
-@app.route("/sources")
-def Sources():
-    return render_template("Sources.html", title="Source")
+@app.route("/dash")
+def dash():
+    if 'user' in session:
+        if session['lavel'] == 1 or 2:
+            print(session['lavel'])
+            content = contents.query.all()
 
-if __name__ == '__main__':
-    app.run()
+            return render_template("dash.html", title="dashboard",content=content)
+
+    return redirect(url_for("login"))
+
+@app.route("/admin")
+def admin():
+    if 'user' in session:
+        if session['lavel'] == 2:
+            users = user.query.all()
+            print(users)
+            return render_template("admin.html", title="Admin Panel",users=users)
+    return redirect(url_for("login"))
+
+
+@app.route("/form", methods=['GET', 'POST'])
+def form():
+    if g.user:
+        if(request.method=='POST'):
+            title  = request.form.get('title')
+            bgimg = request.form.get('bgimg')
+            href1 = request.form.get('href1')
+            href2 = request.form.get('href2')
+            info = request.form.get('info')
+
+            entry=contents(title=title,bgimg=bgimg,href1=href1,href2=href2,info=info)
+            db.session.add(entry)
+            db.session.commit()
+
+        return render_template('form.html', title="Form")
+    return redirect(url_for("login"))
+
+
+@app.before_request
+def before_request():
+    g.user = None
+    if 'user' in session:
+        g.user = session['user']
+
+@app.route("/dash/<string:user_slug>", methods=['GET'])
+def info(user_slug):
+    if 'user' in session:
+        content = contents.query.filter_by(title=user_slug).first()
+        return render_template('info.html',content=content )
+    return redirect(url_for('index'))
+    
+@app.route('/logout')
+def logout():
+    session.pop('user')
+    return redirect(url_for('index'))
+
+@app.route("/sources")
+def Sources():  
+    return render_template("Sources.html", title="Source")
---
+++ flask.sql
@@ -0,0 +1,107 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 05, 2023 at 08:44 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `flask`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contents`
+--
+
+CREATE TABLE `contents` (
+  `no` int(25) NOT NULL,
+  `bgimg` varchar(255) COLLATE latin1_general_cs NOT NULL,
+  `href1` varchar(255) COLLATE latin1_general_cs NOT NULL,
+  `title` text COLLATE latin1_general_cs NOT NULL,
+  `href2` varchar(255) COLLATE latin1_general_cs NOT NULL,
+  `info` text COLLATE latin1_general_cs NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+
+--
+-- Dumping data for table `contents`
+--
+
+INSERT INTO `contents` (`no`, `bgimg`, `href1`, `title`, `href2`, `info`) VALUES
+(0, 'https://tastythailand.com/wp-content/uploads/2013/05/one-piece-film-z.jpg', 'https://www.crunchyroll.com/series/GRMG8ZQZR/one-piece', 'One Piece.', 'https://onepiece.fandom.com/wiki/One_Piece_Wiki', 'Monkey. D. Luffy refuses to let anyone or anything stand in the way of his quest to become the king of all pirates. With a course charted for the treacherous waters of the Grand Line and beyond, this is one captain who\'ll never give up until he\'s claimed the greatest treasure on Earth: the Legendary One Piece!'),
+(2, 'https://cv.bkmkn.kodansha.co.jp/9784063621945/9784063621945_w.jpg', 'https://sanji.to/at-the-mercy-of-the-sky-specials-5427?ref=search', 'At The Mercy of The Sky', 'https://www.imdb.com/title/tt1463151/plotsummary/', 'A visually striking anime about a bookworm-ish high school student who returns to his hometown and joins the local astronomy club. Saku Ooyagi is back, and both he and his childhood friend Mihoshi Akeno are starting school, but they haven\'t seen each other since a painful accident years ago. Now Mihoshi is recruiting members for the astronomy club ... and she\'s fiercely determined to have Saku join the club.'),
+(3, 'https://m.media-amazon.com/images/I/916t-LkwsgL._RI_.jpg', 'https://sanji.to/pocos-udon-world-883?ref=search', 'Poco\'s Udon World.', 'https://myanimelist.net/anime/32673/Udon_no_Kuni_no_Kiniro_Kemari', 'Taking a break from the hustle and bustle of Tokyo, Souta Tawara returns to his hometown in Kagawa. Though his parents are no longer around, his former home and family-owned udon restaurant reminds him of the times his family was still together. Reminiscing about his childhood, Souta enters the udon restaurant and discovers a grimy young boy sleeping.'),
+(5, 'https://i.pinimg.com/originals/0a/7b/0b/0a7b0b4f086c62d90ab3810ac4dbd327.jpg', 'https://www.justwatch.com/in/tv-show/naruto', 'Naruto', 'https://www.crunchyroll.com/series/GYQ4MW246/naruto-shippuden', 'Naruto Uzumaki wants to be the best ninja in the land. He\'s done well so far, but with the looming danger posed by the mysterious Akatsuki organization, Naruto knows he must train harder than ever and leaves his village for intense exercises that will push him to his limits.'),
+(4, 'https://cgtranslations321782266.files.wordpress.com/2018/04/005.jpg', 'https://www.crunchyroll.com/series/GJ0H7Q5V7/konosuba--an-explosion-on-this-wonderful-world', 'KONOSUBA -An Explosion on This Wonderful World!', 'https://www.imdb.com/title/tt27329086', 'One year before a certain useless goddess and NEET extraordinaire hit the scene, Megumin, the \"Greatest Genius of the Crimson Magic Clan,\" is hard at work. Ever since a life-changing encounter in her youth, the young wizard has dedicated her every waking moment to the pursuit of the ultimate offensive magic: Explosion magic. Well, every moment not spent hustling food'),
+(6, 'https://cdn.otakutale.com/wp-content/uploads/2019/11/New-Boku-no-Hero-Academia-Hero-Rising-Visual-Trailer-Revealed-500x400.jpg', 'https://www.crunchyroll.com/series/GYNV9DP2R/my-hero-academia-dubs', 'My Hero Academia', 'https://www.imdb.com/title/tt5626028', 'A superhero-loving boy without any powers is determined to enroll in a prestigious hero academy and learn what it really means to be a hero.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `email` varchar(32) COLLATE latin1_general_cs NOT NULL,
+  `username` varchar(16) COLLATE latin1_general_cs NOT NULL,
+  `password` varchar(512) COLLATE latin1_general_cs NOT NULL,
+  `age` int(11) NOT NULL DEFAULT = 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_cs;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `email`, `username`, `password`) VALUES
+(1, 'admin@anime.com', 'admin', '12345');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `contents`
+--
+ALTER TABLE `contents`
+  ADD PRIMARY KEY (`no`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `contents`
+--
+ALTER TABLE `contents`
+  MODIFY `no` int(25) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
