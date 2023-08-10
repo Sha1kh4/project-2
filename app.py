d237e8f9b642b50172877b607e0af19c3d32d289
Sha1kh4 <117739824+Sha1kh4@users.noreply.github.com>
2023-08-10 16:12:25 +0000

    added 404 page

--- app.py
+++ app.py
@@ -172,3 +172,8 @@ def delete(user_slug):
             return redirect(url_for('dash'))
     else:
         return redirect(url_for('index'))
+
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html',title="404 xD")
